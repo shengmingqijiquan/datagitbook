f8897e4e3cf390175e97751d83ce0620b78e9406
liyu <1127641712@qq.com>
2019-08-03 14:17:09 +0800

    修改配置信息

---
+++ scripts/publish.bat
@@ -0,0 +1,8 @@
+@echo off
+echo [INFO] Publis _book to gh-pages.
+cd ../_book
+git init
+git commit --allow-empty -m "update book"
+git checkout -b gh-pages && git add .
+git commit -am "update book"
+git push git@github.com:shengmingqijiquan/datagitbook gh-pages --force"
